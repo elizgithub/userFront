45033fa452f7b26e5bd978016728304cdeb58306
elizgithub <eacs101@gmail.com>
2018-02-05 16:23:49 +0300

    Commited by Elias from DELL PC

---
+++ src/main/java/com/userfront/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.userfront.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public String UserProfile(){
+        return "profile";
+    }
+}
